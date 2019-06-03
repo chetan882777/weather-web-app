e04092ebf8f189bf4bc4c638e12fd6e2054e9717
chetan882777 <chetan882777@gmail.com>
2019-06-04 02:08:09 +0530

    use geocoding mapbox api

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ request({url:url}, (error, response) => {
     const data = JSON.parse(response.body)
     console.log(data.currently)
 })
+
+const geoUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Barwani.json?access_token=pk.eyJ1IjoiY2hldGFucGF3YXIiLCJhIjoiY2p3Z3R0MDhoMGNxdjQwbGJnajM2ZHo4byJ9.5azLiuRBvbXvy3xE0tDovw"
+
+request({url:geoUrl}, (error, response) => {
+    const data2 = JSON.parse(response.body)
+    const lang = data2.features[0].center[0]
+    const lat = data2.features[0].center[1]
+
+    console.log("lat " + lat + ", lang " + lang)
+})
