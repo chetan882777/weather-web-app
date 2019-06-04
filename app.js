e7696d66f692dad3e6d512374436c579fd15ba7e
chetan882777 <chetan882777@gmail.com>
2019-06-04 13:32:01 +0530

    minute fix

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const request = require("request")
 const geoCode = require("./utils/geocode")
 const forecast = require("./utils/forcast")
 
-if(process.argv.length  < 2){
+if (process.argv.length < 3) {
     return console.log("Name of place required!")
-}
-geoCode(process.argv[2], (error, data) => {
+} else {
+    geoCode(process.argv[2], (error, data) => {
 
-    if(error){
-        return console.log(error)    
-    }
-
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
-        if(error){
+        if (error) {
             return console.log(error)
         }
-        console.log(data.location)
-        console.log(forecastData.data)
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData.data)
+        })
     })
-})
-
-
+}
